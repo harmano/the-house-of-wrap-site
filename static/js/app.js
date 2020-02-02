3093743d408cb7106460460035ee3d04c585a4a4
Harman <Harman007@Ramans-iMac.local>
2020-02-02 17:05:39 -0500

    Updated contact

---
+++ static/js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+ 
+ 
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+  
+ // https://dribbble.com/shots/2955075-Neon-Sign-in-CSS
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+ 
+ 
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+  
+ // https://dribbble.com/shots/2955075-Neon-Sign-in-CSS
+ 
+ 
+ $(document).ready(function() {
+         
+          $("#owl-demo").owlCarousel({
+         
+              navigation : true, // Show next and prev buttons
+              slideSpeed : 300,
+              paginationSpeed : 400,
+              singleItem:true
+         
+              // "singleItem:true" is a shortcut for:
+              // items : 1, 
+              // itemsDesktop : false,
+              // itemsDesktopSmall : false,
+              // itemsTablet: false,
+              // itemsMobile : false
+         
+          });
+         
+        });
